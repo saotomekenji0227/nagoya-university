f5f41298e9fa09cf9348ae262d92b40f3d71011a
nyaso <aaa@aaa>
2017-05-20 04:01:26 -0700

    delete

--- パターン認識/makeperceptron.c
+++ パターン認識/makeperceptron.c
@@ -10,56 +10,66 @@
 double multi(double x[],double w[]);
 
 int main(int argc,char* argv[]){
-  double rho=1.0;
-  double x[P][N];
-  int o[P];
-  double w[C][N+1];
+  double rho=1.0; //1.0倍の補正
+  double x[P][N]; //学習パターンにおける値
+  int o[P]; //クラスの記憶
+  double w[C][N+1]; //重み
 
   int i,j;
   FILE *fp=fopen(argv[1],"r");
   for(i=0;i<P;i++){
     for(j=0;j<N;j++){
-      fscanf(fp,"%lf",&x[i][j]);
+      fscanf(fp,"%lf",&x[i][j]); //x読み出し
     }
-    fscanf(fp,"%d",&o[i]);
+    fscanf(fp,"%d",&o[i]); //o読み出し
   }
   for(i=0;i<C;i++){
     for(j=0;j<N+1;j++){
-      fscanf(fp,"%lf",&w[i][j]);
+      fscanf(fp,"%lf",&w[i][j]); //w読み出し
     }
   }
   fclose(fp);
 
-  int flag=P-1; //最後に更新した時のパターン
+  int flag=-999; //最後に更新した時のパターン
   int p=0;  //現在見ているパターン
   double g[C]; //計算結果
-  while(p!=flag){
-    for(i=0;i<C;i++)
+  while(p!=flag+1){ //一周変わらなくなるまで
+    for(i=0;i<C;i++){ //クラス数g[]を計算
       g[i]=multi(x[p],w[i]);
-    double max=g[0];
-    int index;
-    for(i=1;i<C;i++)
-      if(g[i]>max){
+    }
+    double max=g[o[p]-1]; //最大値計算
+    int index=o[p]-1;
+    for(i=0;i<C;i++){
+      if(i==o[p]-1) continue;
+      else if(g[i]>max-0.00001){
 	max=g[i];
 	index=i;
       }
-    if(index!=o[p]){
+    }
+
+    if(index!=(o[p]-1)){
+      //g[]の配列番号は0からであることに注意
+      //例えばg[0]がg1の値となる
       flag=p;
       w[index][0]--;
       w[o[p]][0]++;
       for(i=1;i<N+1;i++){
-	w[index][i]-=x[p][i-1];
-	w[o[p]][i]+=x[p][i-1];
+	w[index][i]-=x[p][i-1]*rho;
+	w[o[p]-1][i]+=x[p][i-1]*rho;
       }
     }
     p++;
-    if(p==N) p=0;
+    if(p==P){
+      if(flag==-999) break;
+      else p=0;
+    }
   }
  
   fp=fopen(argv[2],"w");
+  fprintf(fp,"%d %d %d\n",N,C,P);
   for(i=0;i<C;i++){
     for(j=0;j<N+1;j++){
-      fprintf(fp,"%d ",w[i][j]);
+      fprintf(fp,"%lf ",w[i][j]);
     }
     fprintf(fp,"\n");
   }
@@ -68,7 +78,8 @@ int main(int argc,char* argv[]){
 }
 
 double multi(double x[],double w[]){
-  int sum=w[0],i;
+  int i;
+  double sum=w[0];
   for(i=0;i<N;i++){
     sum+=x[i]*w[i+1];
   }
