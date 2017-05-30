b9a47f4b6beb649c16ab780873b996a4c91617bc
nyaso <aaa@aaa>
2017-05-30 06:08:27 -0700

    make prototype

--- パターン認識/4/data/makeprototype.c
+++ パターン認識/4/data/makeprototype.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
+#define MAX 1000
 
-
-void readfile(FILE *fp,int *w,int *h,int A[*h][*w]);
-void writefile(FILE *fp,int w,int h,int A[h][w]);
+void readfile(FILE *fp,int *w,int *h,int A[MAX][MAX]);
+void writefile(FILE *fp,int w,int h,int A[MAX][MAX]);
 
 void main(int argc,char *argv[]){
   int i,j,k,l;
@@ -13,37 +13,28 @@ void main(int argc,char *argv[]){
   fscanf(fp,"%d",&w);
   fscanf(fp,"%d",&h);
   fclose(fp);
-  int **data,**tmp;//学習データ読み込み
-  data=(int**)malloc(h*sizeof(int*));
-  tmp=(int**)malloc(h*sizeof(int*));
-  for(i=0;i<h;i++){
-    data[i]=(int*)malloc(w*sizeof(int));
-    tmp[i]=(int*)malloc(w*sizeof(int));
-  }
+  int data[MAX][MAX],tmp[MAX][MAX];//学習データ読み込み
   for(i=0;i<10;i++){
     for(j=0;j<h;j++)
       for(k=0;k<w;k++)
 	data[j][k]=0;
-    
     for(j=0;j<100;j++){
-      sprintf(fileName,"%01d-%02d.dat",i,j);
+      sprintf(fileName,"normal%01d-%02d.dat",i,j);
       fp=fopen(fileName,"r");
       readfile(fp,&w,&h,tmp);
-      //for(k=0;k<h;k++)
-      	//for(l=0;l<w;l++)
-	  //  data[k][l]+=tmp[k][l];
+      for(k=0;k<h;k++)
+      	for(l=0;l<w;l++)
+	  data[k][l]+=tmp[k][l];
       fclose(fp);
     }
-    /*
     sprintf(fileName,"proto%01d.dat",i);
     fp=fopen(fileName,"w");
     writefile(fp,w,h,data);
     fclose(fp);
-    */
-    }
+  }
   return;
 }
-void readfile(FILE *fp,int *w,int *h,int A[*h][*w]){
+void readfile(FILE *fp,int *w,int *h,int A[MAX][MAX]){
   int i,j;
   int tmp;
   fscanf(fp,"%d",w);
@@ -56,7 +47,7 @@ void readfile(FILE *fp,int *w,int *h,int A[*h][*w]){
   }
 }
 
-void writefile(FILE *fp,int w,int h,int A[h][w]){
+void writefile(FILE *fp,int w,int h,int A[MAX][MAX]){
   int i,j;
   fprintf(fp,"%d %d\n",w,h);
   for(i=0;i<h;i++){
